f0b4837c6acd3347ec5d5512ddb723a404207c5d
rukigaki <rukigaki@gmail.com>
2025-08-22 10:59:53 +0300

    Создание единого интерфейса управления функциями

---
+++ aggregate_module/JSONAggregate_interface.py
@@ -0,0 +1,36 @@
+import json
+
+from parser.parser_engine import EngineParse
+from aggregate_module import AverageHandler
+
+
+
+class JSONAggregateInterface:
+    def __init__(self) -> None:
+        self.parsed_args = EngineParse()
+        self.data = self.__prepare_data(file_path=self.parsed_args.get_file_path)
+
+        self.average = AverageHandler()
+        ...
+        self.aggregate_functions = [self.average, ...]
+        self.str_view_agg_func = [str(i) for i in self.aggregate_functions]
+
+
+    def report(self, report_param) -> dict[str, list]:
+        ind = self.str_view_agg_func.index(report_param)
+        return self.aggregate_functions[ind].report(self.data)
+
+
+
+    @staticmethod
+    def __prepare_data(file_path) -> list[dict]:
+        data = []
+
+        with open(file_path) as file:
+            str_jsons = file.readlines()
+            for str_json in str_jsons:
+                dict_obj = json.loads(str_json)
+                data.append(dict_obj)
+
+        return data
+
