955980ff689b550d6febe808a8ceac2f4b2987df
rukigaki <rukigaki@gmail.com>
2025-08-22 11:13:33 +0300

    Внесены правки в описание класса

--- aggregate_module/abc_base.py
+++ aggregate_module/abc_base.py
@@ -7,14 +7,24 @@ class Aggregate(ABC):
     @abstractmethod
     def report(self, data) -> dict[str, list]:
         """
-        Абстрактный метод для создания Операций, которые будут применены для манипуляции с таблицей
-        Example: aggregate, order_by, sorting и т.д.
+        Абстрактный метод для создания логики отображения по агрегирующей функции (реализован - average,
+        но также есть возможность добавить что-то новое по типу (sum, max, min и т.д.))
 
-        :param data: Это распарсенный csv-файл, который является списком словарей
-        Example: [{"brand": "apple", "price": "100", "rating": "4.5"},
-                {"brand": "samsung", "price": "200", "rating": "3.5"}]
+        :param data: Это распарсенный log-файл, который содержит в себе json строки, они преобразованы в нативный для
+        Python тип данных, список из словарей list[dict]
+
+        Example: [
+        {"@timestamp": "2025-06-22T13:57:32+00:00", "status": 200, "url": "/api/context/...", "request_method": "GET", "response_time": 0.024, "http_user_agent": "..."}
+        {"@timestamp": "2025-06-22T13:57:32+00:00", "status": 200, "url": "/api/context/...", "request_method": "GET", "response_time": 0.02, "http_user_agent": "..."}
+        ]
 
 
-        :return: Возвращает уже готовый объект в orchestrator.py, который вызывается в main.py(точка входа)
+        :return: Возвращает tabular_data, это словарь такого типа
+
+        Example: {
+            "handler": list[str],
+            "total": list[int],
+            "avg_response_time": list[float]
+            }
         """
         pass
